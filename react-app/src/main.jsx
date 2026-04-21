3d77e6f23020e4f19e2d20a6e39a4682f995fb69
cvarshini1325 <cvarshini1325@gmail.com>
2026-04-21 10:14:40 +0530

    first commit

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './routers/AppRouter.jsx'
-import { FitnessProvider } from './context/FitnessContext.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FitnessProvider>
-      <App />
-    </FitnessProvider>
+    <App />
   </StrictMode>,
 )
